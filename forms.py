dd20345268f64c263ad104d8ddc0a2e6a2dfc91d
Peter Botsford <petertoddbotsford@gmail.com>
2022-04-16 23:10:33 -0600

    beginning to make edits

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
-
 class MessageForm(FlaskForm):
     """Form for adding/editing messages."""
 
